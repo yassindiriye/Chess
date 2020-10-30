3dd3a069f888979631aebbaea584eaa30f7d1d97
yassindiriye <y.diriye@student.avans.nl>
2020-10-31 00:27:29 +0100

    code layout

---
+++ src/Pieces/Pawn.java
@@ -0,0 +1,19 @@
+package Pieces;
+
+import java.awt.*;
+
+public class Pawn {
+
+    private String name;
+    private Color color;
+
+    public Pawn(String name, Color color){
+        this.name = name;
+        this.color = color;
+    }
+
+
+
+
+
+}
